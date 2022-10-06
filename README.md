b2c1281323866f8ad9ae1b6ac550ed46ed4c3ee6
Akshaj Verma <akshajverma7@gmail.com>
2022-10-06 23:05:59 +0200

    Update readme.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# evocomp
+# Evolutionary Computing
+
+## Installation
+
+Install in a virtual environment (miniconda/anaconda is advised.)
+
+Create and environment.
+
+```
+conda create --name evocomp python=3
+```
+
+Install relevant packages.
+
+```
+pip install tmx numpy pygame
+```
+
+Clone repository. The `evoman/` folder is borrowed from @karinemiras' [evoman_framework](https://github.com/karinemiras/evoman_framework).
+
+```
+git clone https://github.com/theairbend3r/evocomp.git
+```
+
+## Usage
+
+### Run a single experiment
+
+```
+python execute_experiment.py
+```
+
+### Tune hyperparameters (grid search)
+
+```
+python tune_params.py
+```
+
+This will run a grid search based on parameters in the `params.py` file. The grid search uses multiprocessing.
--- tune_params.py
+++ tune_params.py
@@ -1,13 +1,12 @@
 import itertools
 from functools import partial
-from collections import namedtuple
-from concurrent.futures import ProcessPoolExecutor
 from multiprocessing import Pool
+from collections import namedtuple
 
 from evolution import (
-    mutation,
+    mutate,
     crossover,
-    selection,
+    select_for_next_generation,
     fitness,
 )
 from params import params
@@ -48,50 +47,12 @@ if __name__ == "__main__":
                 pc.representation_size_upper_limit,
                 pc.representation_size_lower_limit,
                 pc.num_generations,
-                partial(mutation, method=pc.mutation),
+                partial(mutate, method=pc.mutation),
                 partial(crossover, method=pc.crossover),
-                partial(selection, method=pc.selection),
+                partial(select_for_next_generation, method=pc.selection),
                 partial(fitness, method=pc.fitness),
             )
         )
 
     with Pool(processes=4) as pool:
         pool.starmap(execute_experiment, params_combo_list)
-
-    # with ProcessPoolExecutor() as executor:
-    #     for r in executor.map(execute_experiment, params_combo_list):
-    #         print(r)
-
-    # ParamRow = namedtuple(
-    #     "ParamRow",
-    #     [
-    #         "enemies",
-    #         "population_size",
-    #         "num_hidden_neurons",
-    #         "representation_size",
-    #         "representation_size_upper_limit",
-    #         "representation_size_lower_limit",
-    #         "num_generations",
-    #         "mutation",
-    #         "crossover",
-    #         "selection",
-    #         "fitness",
-    #     ],
-    # )
-
-    # params_combo = [ParamRow(*pc) for pc in params_combo]
-
-    # for pc in params_combo:
-    #     execute_experiment(
-    #         enemies=pc.enemies,
-    #         population_size=pc.population_size,
-    #         num_hidden_neurons=pc.num_hidden_neurons,
-    #         representation_size=pc.representation_size,
-    #         representation_size_upper_limit=pc.representation_size_upper_limit,
-    #         representation_size_lower_limit=pc.representation_size_upper_limit,
-    #         num_generations=pc.num_generations,
-    #         mutation=partial(mutation, method=pc.mutation),
-    #         crossover=partial(crossover, method=pc.crossover),
-    #         selection=partial(selection, method=pc.selection),
-    #         fitness=partial(fitness, method=pc.fitness),
-    #     )
