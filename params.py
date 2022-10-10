29e8f08cb42a7ac933560c90fd31a51495480ab3
Akshaj Verma <akshajverma7@gmail.com>
2022-10-10 16:56:58 +0200

    Add working multiprocessing.

--- params.py
+++ params.py
@@ -1,10 +1,10 @@
 params = {
-    "enemies": [[7, 8], [1, 2, 3]],
-    "population_size": [4, 5],
+    "enemies": [[7, 8]],
+    "population_size": [2, 3],
     "num_hidden_neurons": [10],
-    "num_generations": [5, 10],
-    "mutation": ["mutation_1", "mutation_2"],
-    "crossover": ["onepoint", "uniform"],
-    "selection": ["selection_1", "selection_2"],
-    "fitness": ["fitness_1", "fitness_2"],
+    "num_generations": [2, 3],
+    "mutation": ["random"],
+    "crossover": ["onepoint"],
+    "selection": ["selection_1"],
+    "fitness": ["fitness_1"],
 }
--- tune_params.py
+++ tune_params.py
@@ -1,24 +1,23 @@
+import os
 import itertools
+import subprocess
 from functools import partial
-from multiprocessing import Pool
 from collections import namedtuple
 
 from evolution import (
-    mutate,
-    crossover,
-    select_for_next_generation,
+    create_offspring,
+    select_individuals_for_next_generation,
     fitness,
 )
 from params import params
-from execute_experiment import execute_experiment
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("tag", help="Tag for experiment e.g. ea1")
-    args = parser.parse_args()
+    # import argparse
+    #
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("tag", help="Tag for experiment e.g. ea1")
+    # args = parser.parse_args()
 
     params_combo = list(itertools.product(*[v for v in params.values()]))
 
@@ -40,19 +39,25 @@ if __name__ == "__main__":
 
     params_combo_list = []
     for pc in params_combo:
+        # name experiment/run to save logs.
+        experiment_name = f"enemy_{''.join([str(e) for e in pc.enemies])}-population_size_{pc.population_size}-num_hidden_neurons_{pc.num_hidden_neurons}-num_generations_{pc.num_generations}-mutation_{pc.mutation}-crossover_{pc.crossover}-selection_{pc.selection}-fitness_{pc.fitness}"
+        # experiment_name = f"enemy_{''.join([str(e) for e in pc.enemies])}"
         params_combo_list.append(
             (
-                args.tag,
                 pc.enemies,
                 pc.population_size,
                 pc.num_hidden_neurons,
                 pc.num_generations,
-                partial(mutate, method=pc.mutation),
-                partial(crossover, method=pc.crossover),
-                partial(select_for_next_generation, method=pc.selection),
+                partial(
+                    create_offspring,
+                    mutation_method=pc.mutation,
+                    crossover_method=pc.crossover,
+                ),
+                partial(select_individuals_for_next_generation, method=pc.selection),
                 partial(fitness, method=pc.fitness),
             )
         )
 
-    with Pool(processes=4) as pool:
-        pool.starmap(execute_experiment, params_combo_list)
+        os.system(
+            f"python execute_experiment.py -n {experiment_name} -e {' '.join([str(e) for e in pc.enemies])}",
+        )
